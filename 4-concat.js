7a0f811348ae2c4dfb0fc526f7f63cc018cdf33b
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-07-13 21:10:49 +0300

    made some adjustments

--- 4-concat.js
+++ 4-concat.js
@@ -3,7 +3,7 @@ const arr = process.argv.slice(2);
 if(arr.length == 2){
     console.log(`${arr[0]} is ${arr[1]}`);
 }else if(arr.length == 1){
-    console.log(`${arr[1]} is undefined`);
+    console.log(`${arr[0]} is undefined`);
 }else{
     console.log('undefined is undefined');
 }
