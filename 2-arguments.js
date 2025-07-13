f22856105ccab9da1d7df575aa89f29aa6400f16
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-07-13 17:27:03 +0300

    made adjuxtments

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+arguments = [];
+let j = 0;
+for(let i = 0; i < arguments.length;i++){
+    j += i;
+}
+if(j = 0){
+    console.log("No argument");
+}else if(j = 1){
+    console.log("Argument found");
+}else{
+    console.log("Arguments found");
+}
