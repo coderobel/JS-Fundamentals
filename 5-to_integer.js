e809ae06862b09811ed8b70110cebf8ab53b5b41
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-07-14 12:15:08 +0300

    adjusted

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,6 +1,6 @@
-let arr = process.argv.slice(2);
+let arr = process.argv.slice(2,3);
 
-if(Number(arr[0])){
+if((arr[0])){
     console.log("My number: ", arr[0]);
 }else{
     console.log("Not a number");
