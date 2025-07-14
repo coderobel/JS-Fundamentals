6aa1f5d886f4b303a91667b6f10d0d4bea8b9cfd
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-07-14 13:37:16 +0300

    adjusted

--- 2-arguments.js
+++ 2-arguments.js
@@ -5,6 +5,7 @@ if(arr.join === ""){
     console.log("No argument");
 }//else if(j = 1){
    // console.log("Argument found");
-//}else if(j >= 2){
-  //  console.log("Arguments found");
 //}
+else if(j >= 2){
+    console.log("Arguments found");
+}
