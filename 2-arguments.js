4d5cb364bd8cc31a10500d554338d6a397bdff6f
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-07-14 13:35:57 +0300

    adjusted

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,10 @@
-let argument = process.argv.slice(2);
+let arr = process.argv.slice(2);
 let j = argument.length;
 
-if(argument.join === ""){
+if(arr.join === ""){
     console.log("No argument");
-}else if(j = 1){
-    console.log("Argument found");
-}else if(j >= 2){
-    console.log("Arguments found");
-}
+}//else if(j = 1){
+   // console.log("Argument found");
+//}else if(j >= 2){
+  //  console.log("Arguments found");
+//}
