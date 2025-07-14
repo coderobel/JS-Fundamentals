655fbd4ea8e834694a6e34d2334cac90a272e8ae
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-07-14 12:28:24 +0300

    adjust

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,6 +1,6 @@
 let arr = process.argv.slice(2,3);
 
-if(typeof arr[0] == Number){
+if(typeof Number(arr[0]) === 'number' && !isNaN(Number(arr[0]))){
     console.log("My number:", arr[0]);
 }else{
     console.log("Not a number");
