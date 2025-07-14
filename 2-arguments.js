2b3f62c4317a8b0e896c93f2bedfd0df5962db07
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-07-14 13:38:19 +0300

    adjusted

--- 2-arguments.js
+++ 2-arguments.js
@@ -3,9 +3,9 @@ let j = argument.length;
 
 if(arr.join === ""){
     console.log("No argument");
-}//else if(j = 1){
-   // console.log("Argument found");
-//}
+}else if(j == 1){
+    console.log("Argument found");
+}
 else if(j >= 2){
     console.log("Arguments found");
 }
