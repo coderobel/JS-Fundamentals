1e4357ac86bdd1bbe5813032544444046930be3e
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-07-14 12:07:50 +0300

    adjusted

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 let arr1 = ['C', 'Python', 'Javascript'];
 let arr2 = ['fun', 'cool', 'amazing'];
-result = '';
+let result = '';
 for(let i = 0;i < 3;i++){
     result += `${arr1[i]} is ${arr2[i]}\n`
 }
