6f4f812e92aa56e4f4e35f3965c64a7cae6b966b
coderobel <robel.ugr-4552-16@aau.edu.et>
2025-07-14 14:06:22 +0300

    adjust

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,7 +2,7 @@ let arr = process.argv.slice(2);
 if(typeof Number(arr[0]) == "number"){
     let x = Number(arr[0]);
     let i = 0;
-    while(i < x){
+    while (i < x){
         console.log('Cis fun');
     }
 }
